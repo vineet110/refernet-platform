f8fbdcb43f0a26c7ea816db42491173c284b39e5
Vineet Pandey <vineetjr.11107@gmail.com>
2025-10-15 19:05:06 +0530

    Fix page.tsx default export for App Router

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import Header from "./Header";
-
-import Link from "next/link";
-
 export default function Navbar() {
   return (
     <div className="font-sans">
---
+++ src/app/pages/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PagesPage = () => {
+  return (
+    <div>
+      <h1>Pages</h1>
+    </div>
+  );
+};
+
+export default PagesPage;
